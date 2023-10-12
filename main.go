7cde6176d871db01e1c4d064382f086bec443263
cybercdh <cybercdh@gmail.com>
2023-10-12 13:38:15 +0100

    added option to use a big list of online resolvers, or hardcoded ones

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
 	"strings"
 	"sync"
@@ -16,9 +18,11 @@ import (
 )
 
 var (
-	concurrency int
-	verbose     bool
-	domains     chan check
+	concurrency     int
+	verbose         bool
+	maxRetries      int
+	onlineresolvers bool
+	domains         chan check
 )
 
 type check struct {
@@ -26,13 +30,23 @@ type check struct {
 	Nameserver string
 }
 
-func queryNSEC(domain string, server string, visited *sync.Map) {
-	if _, found := visited.LoadOrStore(domain, true); found {
+type DNSServer struct {
+	IP          string  `json:"ip"`
+	Name        string  `json:"name"`
+	Reliability float64 `json:"reliability"`
+}
+
+func queryNSEC(domain string, servers []string, visited *sync.Map, retryCount int) {
+	// Check if the domain has been visited before
+	if _, found := visited.Load(domain); found {
 		if verbose {
 			fmt.Println("Loop detected, stopping recursion.")
 		}
 		return
 	}
+
+	// Select a random DNS server
+	server := servers[rand.Intn(len(servers))]
 
 	c := new(dns.Client)
 	m := new(dns.Msg)
@@ -41,11 +55,26 @@ func queryNSEC(domain string, server string, visited *sync.Map) {
 	r, _, err := c.Exchange(m, server)
 	if err != nil {
 		if verbose {
-			fmt.Println(err)
+			fmt.Println("Error querying DNS:", err)
 		}
 
+		// Retry with a different DNS server if retry count is not zero
+		if retryCount > 0 {
+			if verbose {
+				fmt.Println("Retrying with a different DNS server...")
+			}
+
+			queryNSEC(domain, servers, visited, retryCount-1)
+		} else {
+			if verbose {
+				fmt.Println("Max retries reached. Skipping", domain)
+			}
+		}
 		return
 	}
+
+	// Mark the domain as visited only after a successful query
+	visited.Store(domain, true)
 
 	for _, ans := range r.Answer {
 		if nsec, ok := ans.(*dns.NSEC); ok {
@@ -54,30 +83,84 @@ func queryNSEC(domain string, server string, visited *sync.Map) {
 			} else {
 				fmt.Println(nsec.NextDomain)
 			}
-			queryNSEC(nsec.NextDomain, server, visited)
+
+			queryNSEC(nsec.NextDomain, servers, visited, retryCount)
 		}
 	}
 }
 
-func worker(visited *sync.Map, wg *sync.WaitGroup) {
+func worker(visited *sync.Map, wg *sync.WaitGroup, servers []string, maxRetries int) {
 	defer wg.Done()
 	for domain := range domains {
-		queryNSEC(domain.Domain, domain.Nameserver, visited)
+		queryNSEC(domain.Domain, servers, visited, maxRetries) // Use domain.Domain here
 	}
+}
+
+func getDNSServers(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	reader := csv.NewReader(resp.Body)
+	// Assuming the CSV has a header row
+	_, err = reader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	var servers []string
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		// Assuming IP is in the first column and reliability is in the tenth column
+		ip := record[0]
+		reliability := record[9]
+
+		// Assuming reliability "1.00" means fully reliable
+		if reliability == "1.00" {
+			servers = append(servers, ip+":53")
+		}
+	}
+
+	return servers, nil
 }
 
 func main() {
 	flag.IntVar(&concurrency, "c", 20, "set the concurrency level")
+	flag.IntVar(&maxRetries, "r", 3, "set the number of retries")
 	flag.BoolVar(&verbose, "v", false, "be more verbose")
+	flag.BoolVar(&onlineresolvers, "o", false, "use online list of DNS resolvers")
 	flag.Parse()
 
-	dnsServers := []string{
-		"1.1.1.1:53",
-		"1.0.0.1:53",
-		"8.8.8.8:53",
-		"8.8.4.4:53",
-		"9.9.9.9:53",
+	var dnsServers []string
+	var err error
+
+	if onlineresolvers {
+		dnsServersURL := "https://public-dns.info/nameservers.csv"
+		dnsServers, err = getDNSServers(dnsServersURL)
+		if err != nil {
+			log.Fatalf("Error fetching DNS servers: %v", err)
+		}
+
+	} else {
+		// if the user doesn't want to use the big online list, then throw a few regular ones in the mix
+		dnsServers = []string{
+			"1.1.1.1:53",
+			"1.0.0.1:53",
+			"8.8.8.8:53",
+			"8.8.4.4:53",
+			"9.9.9.9:53",
+		}
 	}
+
 	rand.Seed(time.Now().UnixNano())
 
 	visited := &sync.Map{}
@@ -87,7 +170,7 @@ func main() {
 
 	for i := 0; i < concurrency; i++ {
 		wg.Add(1)
-		go worker(visited, &wg)
+		go worker(visited, &wg, dnsServers, maxRetries)
 	}
 
 	if _, err := GetUserInput(dnsServers); err != nil {
